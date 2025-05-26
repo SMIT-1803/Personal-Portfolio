2d89213d7ffd18495a9a5a758236d321e40b5181
SMIT-1803 <smitsachin1@gmail.com>
2025-05-26 13:05:36 -0700

    Removed unnecessary code

--- src/JS/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const hamBar = document.querySelector(".hamBar");
-const cross = document.querySelector(".cross");
-
-hamburger.addEventListener("click",()=>{
-    hamBar.classList.toggle("letShow");
-})
